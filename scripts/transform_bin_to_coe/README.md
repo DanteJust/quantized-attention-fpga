bea431fa8c7eeb3f18e6925428bf4c3a0bd31c4f
Tomas Danko <dankotomas22@gmail.com>
2026-04-26 14:07:20 +0200

    feat(scripts): :sparkles: add transform_bin_to_coe script

---
+++ scripts/transform_bin_to_coe/README.md
@@ -0,0 +1,15 @@
+## Transform Binary File to COE
+
+This script converts an input binary file into a `.coe` file, a format commonly used by Vivado.
+
+First create a virtual environment and install the required dependency:
+```bash
+python3 -m venv .venv
+source ./.venv/bin/activate
+pip install numpy
+```
+
+Then run the script:
+```bash
+python script.py
+```
---
+++ scripts/transform_bin_to_coe/script.py
@@ -0,0 +1,32 @@
+import numpy as np
+
+input_bin = input("Input binary file: ")
+output_coe = input("Output coe name: ")
+
+print("1. INT16")
+print("2. INT8")
+print("3. INT4")
+operation_num = int(input("Please select the precision level of the input binary file: "))
+
+match operation_num:
+    case 1:
+        values = np.fromfile(input_bin, dtype=np.int16)
+        formatter = lambda x: f"{np.uint16(x):04X}"
+    case 2:
+        values = np.fromfile(input_bin, dtype=np.int8)
+        formatter = lambda x: f"{np.uint8(x):02X}"
+    case 3:
+        values = np.fromfile(input_bin, dtype=np.int8)
+        formatter = lambda x: f"{(int(x) & 0xF):X}"
+    case _:
+        print("Invalid precision level.")
+        exit(1)
+
+with open(output_coe, "w") as f:
+    f.write("memory_initialization_radix = 16;\n")
+    f.write("memory_initialization_vector =\n")
+
+    for i, v in enumerate(values):
+        hex_val = formatter(v)
+        sep = ",\n" if i < len(values) - 1 else ";\n"
+        f.write(f"{hex_val}{sep}")
